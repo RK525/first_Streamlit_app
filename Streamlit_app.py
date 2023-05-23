a775e51eece06d32110eccbc37389acf91499705
RK525 <134415025+RK525@users.noreply.github.com>
2023-05-23 21:38:00 +0530

    Update Streamlit_app.py
    added breakfast menu hearder and 3 items

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.title("My parents new healthy Diner")
 streamlit.header('BreakFast')
 
 streamlit.text('Omega 3 & BlueBerry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')

f03627e4837d6f4a118a96e0f80db48492707352
RK525 <134415025+RK525@users.noreply.github.com>
2023-05-24 14:03:06 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -2,8 +2,11 @@ import streamlit
 
 streamlit.title("My parents new healthy Diner")
 
-streamlit.header('BreakFast')
+streamlit.header('BreakFast Favorites')
 
-streamlit.text('Omega 3 & BlueBerry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣Omega 3 & BlueBerry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
